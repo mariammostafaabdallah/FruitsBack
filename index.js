13ad6735f4c25958456b02999aeed8f2755fb346
mariammostafaabdallah <132818906+mariammostafaabdallah@users.noreply.github.com>
2024-02-13 01:48:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app=express();
 const mongoose=require("mongoose");
 const productRoute =require("./routes/productRoute");
 const contentRoute =require("./routes/contentRoute");
+const Product=require('./models/Product');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/Shop').then(function(data)
@@ -21,8 +22,8 @@ app.use("/api/Content",contentRoute);
 app.get("/test",(req,res)=>{
     res.json({msg:"hello your app is running"})
 });
-app.get("/allprd", async function (req, res) {
-    const products = await Product.find({});
+app.get("/allprd",  function (req, res) {
+    const products =  Product.find({});
     res.json({
         message: "all product",
         status: 200,
